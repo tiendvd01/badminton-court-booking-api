7f607efd56460c201ffc26c61fc04fff65b556b5
tienchillchill <doanvanduytien@gmail.com>
2025-05-29 19:37:13 +0700

    feat: add notification module

--- src/modules/booking/booking.module.ts
+++ src/modules/booking/booking.module.ts
@@ -5,9 +5,11 @@ import { BookingService } from './booking.service';
 import { Booking } from './entity/booking.entity';
 import { CourtModule } from '@modules/court/court.module';
 import { NotificationsGateway } from 'common/websocket/NotificationsGateway';
+import { NotificationService } from '@modules/notification/notification.service';
+import { NotificationModule } from '@modules/notification/notification.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Booking]), CourtModule],
+  imports: [TypeOrmModule.forFeature([Booking]), CourtModule, NotificationModule],
   controllers: [BookingController],
   providers: [BookingService, NotificationsGateway],
   exports: [BookingService],
